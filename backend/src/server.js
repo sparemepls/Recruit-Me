6f5f0ed6ee4401b8790089c92bde0147d47d8b69
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2026-03-24 16:49:56 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
 import { ENV } from "./lib/env.js";
+import path from "path";
+const __dirname = path.resolve();
 
 const app = express();
 
 console.log(ENV.PORT);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "success from api 4000" });
+// app.get("/", (req, res) => {
+//   res.status(200).json({ msg: "Backend API is running" });
+// });
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "success from api book" });
 });
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success from api health" });
 });
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
+
 app.listen(ENV.PORT, () => console.log("server is running on port", ENV.PORT));
