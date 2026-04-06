f1b5210676dc85879ee55f3f9fd4d6609efc45cf
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2026-04-06 19:34:32 +0530

    feat: enhance landing page with new components and routing, add images, and update styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,22 @@ import HomePage from "./pages/HomePage.jsx";
 import ProblemPage from "./pages/ProblemPage.jsx";
 import { useUser } from "@clerk/react";
 import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage.jsx";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problem"
           element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />}
